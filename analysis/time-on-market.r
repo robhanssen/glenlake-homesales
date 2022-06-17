e124e31c00523c8ed7feb1c7e307ec62d67f70c9
Rob Hanssen <rob.hanssen@outlook.com>
2022-06-17 15:11:53 -0400

    time on market

---
+++ analysis/time-on-market.r
@@ -0,0 +1,48 @@
+library(tidyverse)
+library(lubridate)
+theme_set(theme_light())
+
+load("Rdata/homesales.Rdata")
+
+quarter_summary <-
+    homesales %>%
+    filter(!is.na(saledate), !is.na(timeonmarket)) %>%
+    mutate(quarter = quarter(saledate)) %>%
+    arrange(saledate) %>%
+    group_by(saleyear, quarter) %>%
+    summarize(
+        mean_amount = mean(timeonmarket),
+        median_amount = median(timeonmarket),
+        min_amount = min(timeonmarket),
+        max_amount = max(timeonmarket),
+        .groups = "drop"
+    ) %>%
+    mutate(date = ymd(saleyear * 1e4 + quarter * 3 * 1e2 + 01) - days(15),
+          across(ends_with("amount"), ~ as.numeric(.x)))
+
+lowest_amount <- with(
+    homesales,
+    as.numeric(mean(timeonmarket[saleyear == 2017],
+        na.rm = TRUE
+    )
+    )
+)
+
+quarter_summary %>%
+    ggplot() +
+    aes(x = date) +
+    geom_col(aes(y = mean_amount), alpha = .5, color = "gray50") +
+    geom_ribbon(aes(ymin = min_amount, ymax = max_amount, y = NULL), alpha = .2) +
+    scale_y_continuous(
+        labels = scales::dollar_format(),
+        limits = c(0, NA),
+        sec.axis = sec_axis(~ . / lowest_amount - 1,
+                            labels = scales::percent_format(),
+                            name = "Relative to 2017 mean time on market")
+    ) +
+    labs(
+        y = "Time on market (in days)",
+        x = "Date by year/quarter",
+        caption = "Ribbon represents highest and lower sale price per quarter"
+    ) + 
+    geom_hline(yintercept = lowest_amount, alpha = .7, lty = 3)
