0c0b804f67d043036d7fe777176de8c6a0ae4879
Rob Hanssen <rob.hanssen@outlook.com>
2022-08-03 16:22:36 -0400

    time cluster analysis

---
+++ analysis/time_clustering.r
@@ -0,0 +1,31 @@
+library(tidyverse)
+
+load("Rdata/homesales.Rdata")
+
+setline <- tibble(
+    listingdate = seq(today() - days(100), today(), by = "day"),
+    timeonmarket = today() - listingdate
+)
+
+med2022 <-
+    homesales %>%
+    filter(listingyear == 2022, !is.na(saledate)) %>%
+    pull(timeonmarket) %>%
+    max(.) %>%
+    as.numeric(.)
+
+
+homesales %>%
+    filter(listingyear >= 2021) %>%
+    mutate(onmarket = case_when(
+        is.na(saledate) & is.na(undercontract) ~ "for sale",
+        is.na(saledate) ~ "under contract",
+        TRUE ~ "sold"
+    )) %>%
+    mutate(timeonmarket = as.numeric(timeonmarket)) %>%
+    select(listingdate, onmarket, timeonmarket) %>%
+    ggplot() +
+    aes(x = listingdate, y = timeonmarket, color = onmarket) +
+    geom_point() +
+    geom_line(data = setline, aes(color = NULL), color = "black", alpha = .3) +
+    geom_hline(yintercept = med2022, alpha = .3)
