78b50f40eb256f8447277ccdbb7a5d1c405d731f
Rob Hanssen <rob.hanssen@outlook.com>
2022-08-11 09:46:25 -0400

    graph refinments

--- analysis/time_clustering.r
+++ analysis/time_clustering.r
@@ -29,4 +29,9 @@ homesales %>%
     aes(x = listingdate, y = timeonmarket, color = onmarket) +
     geom_point() +
     geom_line(data = setline, aes(color = NULL), color = "black", alpha = .3) +
-    geom_hline(yintercept = med2022, alpha = .3)
+    geom_hline(yintercept = med2022, alpha = .3) + 
+    scale_x_date(date_label = "%b %Y", date_breaks = "3 months") +
+    labs(x = "Listing date",
+        y = "Time on market (in days)",
+        color = "Status") + 
+    theme_light()
