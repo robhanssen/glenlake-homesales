b1e705d2a163fb46900f69b8493e4eb4243c82dc
Rob Hanssen <rob.hanssen@outlook.com>
2022-10-27 11:43:33 -0400

    initial record

---
+++ analysis/homesale-rate.r
@@ -0,0 +1,30 @@
+library(tidyverse)
+library(lubridate)
+
+load("Rdata/homesales.Rdata")
+
+min_date <- lubridate::ceiling_date(min(dat$saledate, na.rm = TRUE), unit = "month")
+max_date <- lubridate::ceiling_date(lubridate::today(), unit = "month")
+date_range <- seq(min_date, max_date, "month")
+
+
+period_lenght <- 3 # months
+
+map_dfr(
+    date_range,
+    ~ tibble(
+        date = .x,
+        sold = homesales %>% filter(
+            saledate < .x,
+            saledate > .x - months(period_lenght)
+        ) %>%
+            nrow()
+    )
+) %>% 
+ggplot +
+    aes(date, sold) +
+    geom_point() +
+    geom_line() +
+    geom_smooth(method = "loess") +
+    scale_y_continuous(breaks = 4 * 0:100) +
+    theme_light()
