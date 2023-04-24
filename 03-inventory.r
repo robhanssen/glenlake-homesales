8c507091075eeaeb0a77e322c79cbb3550ae296a
Rob Hanssen <rob.hanssen@outlook.com>
2023-04-24 15:24:25 -0400

    added CDF analysis

--- 03-inventory.r
+++ 03-inventory.r
@@ -110,3 +110,79 @@ inventorycalc %>%
         theme(legend.position = "none")
 
 ggsave("graphs/home-inventory.png", width = 8, height = 8)
+
+#
+# CDF for inventory
+#
+#
+
+pdf_norm <- function(x, mu, sig) {
+        .5 * (1 + pracma::erf((x - mu) / (sqrt(2) * sig)))
+}
+
+pdf_lognorm <- function(x, mu, sig) {
+    .5 * (1 + pracma::erf((log(x) - mu) / (sqrt(2) * sig)))
+}
+
+invsoft <-
+        inventorycalc %>%
+        right_join(all_dates) %>%
+        arrange(date) %>%
+        fill(inventory) %>%
+        filter(year(date) > 2017)
+
+minventory <- (min(invsoft$inventory) %/% 10) * 10
+maxventory <- (max(invsoft$inventory) %/% 10 + 1) * 10
+
+mean_inv <- mean((invsoft$inventory))
+sd_inv <- sd((invsoft$inventory))
+
+logmean_inv <- mean(log(invsoft$inventory))
+logsd_inv <- sd(log(invsoft$inventory))
+
+
+inv_cdf <-
+        tibble(
+                inv = seq(minventory, maxventory, 1),
+                cdf = map_dbl(inv, ~ nrow(invsoft %>% filter(inventory <= .x)) / nrow(invsoft)), # nolint
+                cdf_simul_log = map_dbl(inv, ~ pdf_lognorm(.x, logmean_inv, logsd_inv)), # nolint
+                cdf_simul_norm = map_dbl(inv, ~ pdf_norm(.x, mean_inv, sd_inv)) # nolint
+        )
+
+mid_point <-
+        tibble(y = c(.1, .5, .9, .99)) %>%
+        mutate(x = map(
+                y,
+                ~ approx(inv_cdf$cdf, inv_cdf$inv, .x)$y
+        )) %>%
+        unnest(x)
+
+inv_cdf_graph <-
+        inv_cdf %>%
+        ggplot(aes(inv, cdf)) +
+        geom_point(alpha = .2, size = 2) +
+        geom_line(aes(y = cdf_simul_log),
+                linewidth = 2, alpha = .3,
+                color = "gray70"
+        ) +
+        geom_point(
+                data = mid_point,
+                aes(x, y), shape = 3,
+                size = 15, color = "gray50"
+        ) + 
+        scale_y_continuous(
+                        limits = c(0,1),
+                        breaks = c(0, seq(.1, .9, .2), 1),
+                        labels = scales::label_percent()) +
+        scale_x_continuous(breaks = seq(0, maxventory, 5)) +
+        labs(x = "Inventory", y = "",
+             title = "Cumulative density function of homes in inventory at any given moment",
+             caption = "Based on sales from 2018 and later") +
+        theme(
+                panel.grid.minor = element_blank(),
+                plot.caption.position = "plot",
+                plot.caption = element_text(hjust = 0),
+                plot.title.position = "plot"
+        )
+
+ggsave("graphs/homeinventory_cdf.png", width = 7, height = 5, plot = inv_cdf_graph)
