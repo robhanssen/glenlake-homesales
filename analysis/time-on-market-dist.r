2a0a9bb5e2fe18ed16b4a9626f1af042cbb1d26a
Rob Hanssen <rob.hanssen@outlook.com>
2023-05-27 19:11:33 -0400

    added sale amount cdf

--- analysis/time-on-market-dist.r
+++ analysis/time-on-market-dist.r
@@ -32,8 +32,6 @@ perc_markers <-
         timeonmarket = approx(time_cdf$time_cdf, time_cdf$rng, xout = perc)$y
     )
 
-log10(1:10)
-
 time_cdf_g <-
     time_cdf %>%
     ggplot(aes(rng, time_cdf)) +
@@ -57,4 +55,56 @@ time_cdf_g <-
 ggsave("graphs/time_on_market_cdf.png",
     height = 4, width = 6,
     plot = time_cdf_g
+)
+
+#
+# home sale amount
+# 
+
+sale_cdf <-
+    tibble(
+        rng = seq(140e3, max(filtered_homesales$amount, na.rm = TRUE), 10e3),
+        sale_cdf = map_dbl(
+            rng,
+            ~ nrow(filtered_homesales %>% filter(amount <= .x)) /
+                nrow(filtered_homesales)
+        )
+    )
+
+last10 <-
+    filtered_homesales %>%
+    filter(saleyear == year(today())) %>%
+    select(amount) %>%
+    mutate(
+        perc = approx(sale_cdf$rng, sale_cdf$sale_cdf, xout = amount)$y
+    )
+
+perc_markers <-
+    tibble(perc = c(.05, .25, .5, .75, .95)) %>%
+    mutate(
+        amount = approx(sale_cdf$sale_cdf, sale_cdf$rng, xout = perc)$y
+    )
+
+sale_cdf_g <-
+    sale_cdf %>%
+    ggplot(aes(rng, sale_cdf)) +
+    geom_vline(
+        xintercept = perc_markers$amount[perc_markers$perc == .5],
+        linewidth = 2, alpha = .1, color = "gray10"
+    ) +
+    geom_line(linewidth = 2, alpha = .3, color = "gray20") +
+    scale_y_continuous(labels = scales::percent_format()) +
+    scale_x_continuous(
+            labels = scales::dollar_format()
+    ) +
+    geom_point(data = last10, aes(x = amount, y = perc), color = "red") +
+    geom_point(
+        data = perc_markers, aes(x = amount, y = perc),
+        color = "gray30", shape = 3, size = 5
+    ) +
+    labs(x = "Sale price (in USD)", y = "Cumulative Density Function")
+
+ggsave("graphs/sale_amount_cdf.png",
+    height = 4, width = 6,
+    plot = sale_cdf_g
 )
