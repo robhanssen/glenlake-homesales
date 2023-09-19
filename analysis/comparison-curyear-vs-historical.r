4508780baf73088f028ea2268f41ed8f26fc570d
Rob Hanssen <rob.hanssen@outlook.com>
2023-09-19 15:26:08 -0400

    last updates, titles, referencing, linting

--- analysis/comparison-curyear-vs-historical.r
+++ analysis/comparison-curyear-vs-historical.r
@@ -1,6 +1,12 @@
 library(tidyverse)
+library(patchwork)
 
-theme_set(theme_light())
+theme_set(theme_light() +
+    theme(
+        panel.grid.minor = element_blank(),
+        plot.title.position = "plot",
+        plot.caption.position = "plot"
+    ))
 
 load("Rdata/homesales.Rdata")
 
@@ -9,88 +15,91 @@ this_year <- year(today())
 years <- unique(na.exclude(homesales$saleyear))
 years <- years[which(years != this_year)]
 
-
-ttest <-
-    homesales %>%
-    mutate(cur_year = ifelse(saleyear == this_year,
-        "thisyear", "historical"
-    )) %>%
-    drop_na(cur_year) %>%
-    t.test(amount ~ cur_year, alternative = "two.sided", data = .) %>%
-    broom::tidy()
-
-
-p_dist <-
-    homesales %>%
-    mutate(cur_year = ifelse(saleyear == this_year, saleyear, "historical")) %>%
-    drop_na(cur_year) %>%
-    group_by(cur_year) %>%
-    ggplot(aes(x = amount, y = cur_year)) +
-    geom_jitter() +
-    ggridges::geom_density_ridges()
-
-
-
-homesales %>%
-    mutate(cur_year = ifelse(saleyear == this_year,
-        "thisyear", "historical"
-    )) %>%
-    drop_na(cur_year) %>%
-    t.test(timeonmarket ~ cur_year, alternative = "two.sided", data = .)
-
-
-map_df(
-    years,
-    ~ bind_cols(tibble(year = .x), homesales %>%
-        filter(saleyear %in% c(.x, this_year)) %>%
-        mutate(cur_year = ifelse(saleyear == this_year,
-            "thisyear", saleyear
-        )) %>%
-        t.test(amount ~ cur_year, data = .) %>%
-        broom::tidy())
-) %>%
-    ggplot(aes(x = year)) +
-    geom_point(aes(y = estimate1 - estimate2)) +
-    geom_errorbar(aes(ymin = conf.high, ymax = conf.low), width = .3) +
-    scale_y_continuous(labels = scales::label_dollar()) +
-    geom_text(aes(
-        x = year, y = conf.high + 1e4,
-        label = scales::pvalue(p.value)
-    )) +
-    geom_hline(yintercept = 0, linetype = "dashed") +
-    labs(
-        x = "", y = glue::glue("Difference in saleprice"),
-        title = glue::glue(
-            "Difference in saleprice between",
-            " indicated year and {this_year}"
-        ),
-        subtitle = "t.test p-value shows significance"
+mean_curyear <-
+    with(
+        homesales,
+        mean(amount[saleyear == this_year], na.rm = TRUE)
     )
 
-map_df(
-    years,
-    ~ bind_cols(tibble(year = .x), homesales %>%
-        filter(saleyear %in% c(.x, this_year)) %>%
-        mutate(
-            cur_year = ifelse(saleyear == this_year, "thisyear", saleyear),
-            timeonmarket = as.numeric(timeonmarket)
-        ) %>%
-        t.test(timeonmarket ~ cur_year, data = .) %>%
-        broom::tidy())
-) %>%
+meantime_curyear <-
+    with(
+        homesales,
+        as.numeric(mean(timeonmarket[saleyear == this_year], na.rm = TRUE))
+    )
+
+
+
+
+salesprice_g <-
+    map_df(
+        years,
+        ~ bind_cols(tibble(year = .x), homesales %>%
+            filter(saleyear %in% c(.x, this_year)) %>%
+            mutate(cur_year = ifelse(saleyear == this_year,
+                "thisyear", saleyear
+            )) %>%
+            t.test(amount ~ cur_year, data = .) %>%
+            broom::tidy())
+    ) %>%
     ggplot(aes(x = year)) +
-    geom_point(aes(y = estimate1 - estimate2)) +
-    geom_errorbar(aes(ymin = conf.high, ymax = conf.low), width = .3) +
+    geom_point(aes(y = estimate1 - estimate2 + mean_curyear)) +
+    geom_errorbar(
+        aes(
+            ymin = conf.high + mean_curyear,
+            ymax = conf.low + mean_curyear
+        ),
+        width = .3
+    ) +
+    scale_y_continuous(labels = scales::label_dollar(),
+            sec.axis = sec_axis(~ . / mean_curyear - 1, labels = scales::percent_format())) +
     geom_text(aes(
-        x = year, y = conf.high + 5,
+        x = year, y = mean_curyear + conf.high + 1e4,
         label = scales::pvalue(p.value)
     )) +
-    geom_hline(yintercept = 0, linetype = "dashed") +
+    geom_hline(yintercept = mean_curyear, linetype = "dashed") +
     labs(
-        x = "", y = glue::glue("Difference in time on market"),
+        x = "", y = glue::glue("Saleprice"),
         title = glue::glue(
-            "Difference in time on market between",
-            " indicated year and {this_year}"
+            "Saleprice comparison between",
+            " {this_year} and other years"
         ),
-        subtitle = "t.test p-value shows significance"
     )
+
+time_g <-
+    map_df(
+        years,
+        ~ bind_cols(tibble(year = .x), homesales %>%
+            filter(saleyear %in% c(.x, this_year)) %>%
+            mutate(
+                cur_year = ifelse(saleyear == this_year, "thisyear", saleyear),
+                timeonmarket = as.numeric(timeonmarket)
+            ) %>%
+            t.test(timeonmarket ~ cur_year, data = .) %>%
+            broom::tidy())
+    ) %>%
+    ggplot(aes(x = year)) +
+    geom_point(aes(y = estimate1 - estimate2 + meantime_curyear)) +
+    geom_errorbar(
+        aes(
+            ymin = conf.high + meantime_curyear,
+            ymax = conf.low + meantime_curyear
+        ),
+        width = .3
+    ) +
+    geom_text(aes(
+        x = year, y = conf.high + 8 + meantime_curyear,
+        label = scales::pvalue(p.value)
+    )) +
+    geom_hline(yintercept = 0 + meantime_curyear, linetype = "dashed") +
+    labs(
+        x = "", y = glue::glue("Time on market"),
+        title = glue::glue(
+            "Time on market comparison between",
+            " {this_year} and other years"
+        ),
+    )
+
+ggsave("graphs/time-price-comparison.png",
+    width = 8, height = 6,
+    plot = salesprice_g / time_g
+)
