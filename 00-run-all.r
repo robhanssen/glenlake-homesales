e18b85099d0153c29fdcaa851ceafcb8c9e3df58
Rob Hanssen <rob.hanssen@outlook.com>
2024-02-20 12:06:12 -0500

    moving stuff

--- 00-run-all.r
+++ 00-run-all.r
@@ -1,17 +1,23 @@
 # added comment in de v
 source("02-import-data.r")
-source("03-listings-and-sales.r")
-source("03-inventory.r")
 
-source("03-sales-price.r")
-source("03-time-on-market.r")
-source("04-write-data.r")
+scripts <-
+    list.files(path = "scripts", pattern = "*.r$", full.names = TRUE)
 
-source("05-find_resales.r")
-source("06-dashboard.r")
-source("05-predictions.r")
-source("market-fundamentals.r")
-source("market-fundamentals-by-phase.r")
-source("more-predictions.r")
-source("07-homeprices.r")
-source("comparison-curyear-vs-historical.r")
+purrr::walk(scripts, source)
+
+# source("03-listings-and-sales.r")
+# source("03-inventory.r")
+
+# source("03-sales-price.r")
+# source("03-time-on-market.r")
+# source("04-write-data.r")
+
+# source("05-find_resales.r")
+# source("06-dashboard.r")
+# source("05-predictions.r")
+# source("market-fundamentals.r")
+# source("market-fundamentals-by-phase.r")
+# source("more-predictions.r")
+# source("07-homeprices.r")
+# source("comparison-curyear-vs-historical.r")
