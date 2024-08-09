20921dbea03947ea906784cb6e5ac089cb2d8918
Rob Hanssen <rob.hanssen@outlook.com>
2024-08-09 12:54:11 -0400

    double graph added

--- montecarlo/montecarlo7parallel.r
+++ montecarlo/montecarlo7parallel.r
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(ggplot2)
 library(ggridges)
+library(patchwork)
 library(furrr)
 theme_set(theme_light())
 
@@ -9,7 +10,7 @@ plan(multisession)
 load("Rdata/homesales.Rdata")
 
 period_list <- c(90, 120, 180, 360, 720)
-niter_default <- 25000
+niter_default <- 10000
 
 # estimator of time between sales and listings modeled as exponential distribution
 time_estimator <- function(dat, var, period, lambda = .1) {
@@ -64,7 +65,7 @@ simul_sale <- function(dat, period, n_iter = niter_default) {
 
     last_sale <- max(dat$saledate, na.rm = TRUE)
 
-    simul_sale_values <- furrr::future_map_dbl(seq_len(n_iter), \(n) {
+    simul_sale_values <- furrr::future_map(seq_len(n_iter), \(n) {
         sale_simul <- round(rexp(365, rate = sale_time_est))
         price_simul <- exp(rnorm(365, mean = price_est["mean0"], sd = price_est["sd0"]))
 
@@ -73,49 +74,31 @@ simul_sale <- function(dat, period, n_iter = niter_default) {
         est_sales_num <- length(next_sales[next_sales < ceiling_date(today(), unit = "year")])
 
         total_simul_sales <-
-            sum(dat$amount[year(dat$saledate) == year(today())], na.rm = TRUE) +
             sum(price_simul[seq_len(est_sales_num)])
 
-        total_simul_sales
+        list(price = total_simul_sales, number = est_sales_num)
     }, .options = furrr_options(seed = TRUE))
 
     simul_sale_values
 }
 
-single_simul_sale <- function(dat, period, n_iter = niter_default) {
-    sale_time_est <- time_estimator(dat, saledate, period)
-    price_est <- price_estimator(dat, period = period)
+current_sales <- sum(homesales$amount[year(homesales$saledate) == year(today())], na.rm = TRUE)
 
-    last_sale <- max(dat$saledate, na.rm = TRUE)
-
-    simul_sale_values <- map_dbl(seq_len(n_iter), \(n) {
-        sale_simul <- round(rexp(365, rate = sale_time_est))
-        price_simul <- exp(rnorm(365, mean = price_est["mean0"], sd = price_est["sd0"]))
-
-        next_sales <- last_sale + cumsum(sale_simul)
-
-        est_sales_num <- length(next_sales[next_sales < ceiling_date(today(), unit = "year")])
-
-        total_simul_sales <-
-            sum(dat$amount[year(dat$saledate) == year(today())], na.rm = TRUE) +
-            sum(price_simul[seq_len(est_sales_num)])
-
-        total_simul_sales
-    })
-
-    simul_sale_values
-}
+current_num <- sum(!is.na(homesales$amount[year(homesales$saledate) == year(today())]))
 
 multip <- future_map_dfr(period_list, \(p) {
     tibble(
         period = p,
-        price = simul_sale(homesales, p, n_iter = niter_default)
+        out = simul_sale(homesales, p, n_iter = niter_default)
     )
-}, .options = furrr_options(seed = TRUE))
+}, .options = furrr_options(seed = TRUE)) %>%
+    unnest_wider(out) %>%
+    mutate(
+        price = price + current_sales,
+        number = number + current_num
+    )
 
-current_sales <- sum(homesales$amount[year(homesales$saledate) == year(today())], na.rm = TRUE)
-
-dist_g <- 
+dist_g <-
     ggplot(multip) +
     aes(x = price, y = factor(period), fill = factor(period)) +
     geom_density_ridges(alpha = .2) +
@@ -127,9 +110,27 @@ dist_g <-
     ) +
     scale_x_continuous(
         labels = scales::dollar_format(scale = 1e-6, suffix = " M"),
-        breaks = 1e6 * 0:20
+        breaks = 2e6 * 0:20
     ) +
     labs(x = "Expected market value", y = "Predictor period length (in days)") +
     theme(legend.position = "none")
 
-ggsave("montecarlo/mc-par7.png", plot = dist_g)
+num_g <-
+    ggplot(multip) +
+    aes(x = number, y = factor(period), fill = factor(period)) +
+    geom_density_ridges(alpha = .2) +
+    geom_vline(
+        xintercept = current_num,
+        linewidth = 2,
+        alpha = .2,
+        color = "gray50"
+    ) +
+    scale_x_continuous(
+        # labels = scales::dollar_format(scale = 1e-6, suffix = " M"),
+        # breaks = 1e6 * 0:20
+    ) +
+    labs(x = "Expected sales count", y = "Predictor period length (in days)") +
+    theme(legend.position = "none")
+
+
+ggsave("montecarlo/mc-par7.png", plot = dist_g + num_g)
