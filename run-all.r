ae73401630b4f191452dab7824e7be6e08938f8e
Rob Hanssen <rob.hanssen@outlook.com>
2024-12-05 10:02:06 -0500

    added safe_source

--- run-all.r
+++ run-all.r
@@ -1,7 +1,9 @@
 # import all data first
 source("02-import-data.r")
 
+safe_source <- safely(source, quiet = FALSE)
+
 scripts <-
     list.files(path = "scripts", pattern = "\\.r$", full.names = TRUE)
 
-purrr::walk(scripts, source)
+purrr::walk(scripts, safe_source)
